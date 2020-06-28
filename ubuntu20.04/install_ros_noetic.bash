9575c3e206c09670373a22e982c0044466ffae93
Kazuya Iriguchi <baskin.robbins1945@gmail.com>
2020-06-29 00:35:06 +0900

    add: apt install option

--- ubuntu20.04/install_ros_noetic.bash
+++ ubuntu20.04/install_ros_noetic.bash
@@ -4,8 +4,8 @@ sudo apt update && sudo apt install curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update
-sudo apt install ros-foxy-ros-base
+sudo apt install ros-foxy-ros-base -y
 source /opt/ros/foxy/setup.bash
 
 # install argcomplete
-sudo apt install python3-argcomplete
+sudo apt install python3-argcomplete -y
